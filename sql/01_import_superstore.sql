5674aedbcdfec77f1725b38108fc2a4a6a991ff0
Michael Pham <59970071+michaelapham@users.noreply.github.com>
2026-05-21 12:58:19 -0500

    01_import_superstore REIMPORT
    Went from Table Import Wizard to LOAD DATA INFILE. Reasoning: Table Import Wizard caused some sort of column misalignment, leading to data corruption. LOAD DATA INFILE (using CHARACTER SET latin1) resolved the issue.

--- sql/01_import_superstore.sql
+++ sql/01_import_superstore.sql
@@ -13,28 +13,66 @@ USE superstore_sales;
 -- and verify no data loss.
 -- ================================================
 
--- 1.1 Verify table imported
+-- 1.1 Create a new table, superstore_raw, with snake_case
+-- columns and all TEXT types.
+CREATE TABLE superstore_raw (
+    row_id TEXT,
+    order_id TEXT,
+    order_date TEXT,
+    ship_date TEXT,
+    ship_mode TEXT,
+    customer_id TEXT,
+    customer_name TEXT,
+    segment TEXT,
+    country TEXT,
+    city TEXT,
+    state TEXT,
+    postal_code TEXT,
+    region TEXT,
+    product_id TEXT,
+    category TEXT,
+    sub_category TEXT,
+    product_name TEXT,
+    sales TEXT,
+    quantity TEXT,
+    discount TEXT,
+    profit TEXT
+);
+
+
+-- 1.2 Rename original raw csv to "superstore.csv", and copy it into
+-- MySQL file path. Then load the data into superstore_raw.
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/superstore.csv'
+INTO TABLE superstore_raw
+CHARACTER SET latin1
+FIELDS TERMINATED BY ','
+OPTIONALLY ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+
+-- 1.3 Verify table imported
 SELECT * 
 FROM superstore_raw
 LIMIT 10;
 
--- Finding: Original table imported, with TEXT types.
+-- Finding: Import seems to have been a success so far.
 
 
--- 1.2 Check number of rows of superstore_raw. Should equal original 9994 rows.
+-- 1.4 Check number of rows of superstore_raw. Should equal original 9994 rows.
 SELECT COUNT(*)
 FROM superstore_raw;
 
 -- Finding: superstore_raw contains all original 9994 rows.
 
 
--- 1.3 View all columns and types.
+-- 1.5 View all columns and types.
 DESCRIBE superstore_raw;
 
--- Finding: All original columns, all TEXT types.
+-- Finding: All snake_case columns, all TEXT types and nullable.
 
 
--- 1.4 Verify number of columns in superstore_raw vs original superstore table (21).
+-- 1.6 Verify number of columns in superstore_raw vs original superstore table (21).
 SELECT COUNT(*)
 FROM INFORMATION_SCHEMA.COLUMNS
 WHERE TABLE_NAME = 'superstore_raw';
@@ -42,11 +80,19 @@ WHERE TABLE_NAME = 'superstore_raw';
 -- Finding: Raw and original table number of columns match (21).
 
 
--- 1.5 Verify no empty rows were introduced during import.
+-- 1.7 Check that all discounts are between 0 and 1.
+SELECT DISTINCT discount 
+FROM superstore_raw 
+ORDER BY discount;
+
+-- Finding: All discounts between 0 and 1 (0, 0.8).
+
+
+-- 1.8 Verify no empty rows were introduced during import.
 SELECT COUNT(*)
 FROM superstore_raw
-WHERE `Order ID` IS NULL 
-   OR `Order Date` IS NULL 
+WHERE order_id IS NULL 
+   OR order_date IS NULL 
    OR Sales IS NULL;
 
 -- Finding: 0 null values in key columns. Import is clean.
